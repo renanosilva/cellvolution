3c57841a0d1caba796033e11d4a972236cb14a71
Eduarda Pinho <eduardapinho70@gmail.com>
2024-06-14 16:58:09 -0300

    Corrigindo erro que destruía o play

--- RPG/Assets/Resources/Scripts/AudioManager.cs
+++ RPG/Assets/Resources/Scripts/AudioManager.cs
@@ -7,25 +7,24 @@ public class AudioManager : MonoBehaviour
     // Declaração de uma variável pública que armazenará a referência a um componente AudioSource.
     public AudioSource audioSource;
 
-    public static AudioManager Instance { get; private set; }  // MODIFICADO
-
+    // Propriedade estática para acessar a instância do AudioManager
+    public static AudioManager Instance { get; private set; }
 
     // Awake() é chamado quando o script é inicializado.
     private void Awake()
     {
-        if (Instance == null) // MODIFICADO
+        if (Instance == null)
         {
-            // Se não estiver, define esta instância como a instância do Singleton e a torna persistente entre cenas.
+            // Se não houver uma instância, define esta instância como a instância do Singleton e a torna persistente entre cenas.
             Instance = this;
             DontDestroyOnLoad(gameObject);
             audioSource = GetComponent<AudioSource>();
         }
-        else // MODIFICADO
+        else
         {
             // Se já houver uma instância, destrói esta nova instância para manter apenas uma.
-            Destroy(gameObject); // MODIFICADO
+            Destroy(gameObject);
         }
-
     }
 
     // Método público que reproduz um AudioClip recebido como parâmetro.
@@ -37,13 +36,13 @@ public class AudioManager : MonoBehaviour
         audioSource.Play();
     }
 
-    // Método público para definir o volume do AudioSource. // MODIFICADO
+    // Método público para definir o volume do AudioSource.
     public void SetVolume(float volume)
     {
         audioSource.volume = volume;
     }
 
-    // Método público para obter o volume atual do AudioSource. // MODIFICADO
+    // Método público para obter o volume atual do AudioSource.
     public float GetVolume()
     {
         return audioSource.volume;
