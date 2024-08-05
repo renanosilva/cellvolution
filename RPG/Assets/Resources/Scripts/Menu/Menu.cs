aeea772672f1f9110f0d57e186b3627690124e41
Eduarda Pinho <eduardapinho70@gmail.com>
2024-08-05 11:50:05 -0300

    Corrigindo bug do som no executável

--- RPG/Assets/Resources/Scripts/Menu/Menu.cs
+++ RPG/Assets/Resources/Scripts/Menu/Menu.cs
@@ -9,8 +9,7 @@ public class Menu : MonoBehaviour
 
     float GetVol(float vol)
     {
-        float newVol = 0;
-        newVol = 20 * Mathf.Log10(vol);
+        float newVol = 20 * Mathf.Log10(vol);
         if (vol <= 0)
         {
             newVol = -80;
@@ -18,18 +17,42 @@ public class Menu : MonoBehaviour
         return newVol;
     }
 
+    // Funções para ajustar os volumes e salvar as configurações
     public void SetMasterVol(float vol)
     {
         mixer.SetFloat("MasterVol", GetVol(vol));
+        PlayerPrefs.SetFloat("MasterVol", vol);
+        PlayerPrefs.Save();
     }
 
     public void SetMusicVol(float vol)
     {
         mixer.SetFloat("MusicVol", GetVol(vol));
+        PlayerPrefs.SetFloat("MusicVol", vol);
+        PlayerPrefs.Save();
     }
 
     public void SetSFXVol(float vol)
     {
         mixer.SetFloat("SFXVol", GetVol(vol));
+        PlayerPrefs.SetFloat("SFXVol", vol);
+        PlayerPrefs.Save();
     }
-}
+
+    // Função para carregar as configurações de volume ao iniciar o jogo
+    void LoadVolumeSettings()
+    {
+        float masterVol = PlayerPrefs.GetFloat("MasterVol", 1.0f);
+        float musicVol = PlayerPrefs.GetFloat("MusicVol", 1.0f);
+        float sfxVol = PlayerPrefs.GetFloat("SFXVol", 1.0f);
+
+        SetMasterVol(masterVol);
+        SetMusicVol(musicVol);
+        SetSFXVol(sfxVol);
+    }
+
+    void Start()
+    {
+        LoadVolumeSettings();
+    }
+} 
